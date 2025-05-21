a259d90b3a1118d4a37a2aa9b1dce4fdba520530
Atour <28668597+amousavigourabi@users.noreply.github.com>
2025-05-21 15:18:58 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,4 +8,3 @@ apt-get install -y rust cargo
 apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 apt-get install -y protobuf-compiler
-curl https://sh.rustup.rs -sSf | sh -s -- -y
