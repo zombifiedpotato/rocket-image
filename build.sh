8182290ec0e9f1b348406e543c7dac007e6c1ce2
Atour <28668597+amousavigourabi@users.noreply.github.com>
2025-05-21 15:09:59 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,7 +9,3 @@ apt-get install -y apt-transport-https ca-certificates curl software-properties-
 apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 apt-get install -y protobuf-compiler
 curl https://sh.rustup.rs -sSf | sh -s -- -y
-
-# build
-cd rocket-interceptor
-./build.sh
